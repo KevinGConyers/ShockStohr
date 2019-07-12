9614822a0e56315a4dcf880c5820da20c32cf4c0
Kevin <KevinGConyers@gmail.com>
2019-07-12 17:31:19 -0500

    Updated git ignore

--- main.py
+++ main.py
@@ -4,19 +4,52 @@ import argparse
 def handleArgs():
 # Allowed arguments are
 # -f --file [filename] points to the file containing the encrypted data, if not provided a default file is produced in /home/$user/.ShockStohr/defaultstorage
-# -c --clip if this flag is given the password is put on the clipboard using xclip
-# -C --comm [command] if this flag is given, the command is ran with the provided password [EXPERIMENTAL]
+# -m --mode [mode] chose the moder to operate in. The possible modes are clip, add, and comm. Clip retrieves a password and puts in on your clipboard, add adds 
+#   a password to the data file and comm runs a command with the username and password [Not yet implemented]
+# -u [username] the user name to either retrieve the password for, or to store the password under
+# -p [password] the password to store
+# -n [Note] a note to help indentify the password
     parser = argparse.ArgumentParser(description='Command line password manager')
-    parser.add_argument('-f', '--filename', nargs='?', dest='filename', help='provide a filename, if not provided, a default is used')
-    parser.add_argument('-m', '--mode', dest='mode', help='sets the mode.', required='true')
-    args = vars(parser.parse_args(sys.argv[1:]))
-    if args["filename"] is 'None':
-        args["filename"] = 'default'
+    parser.add_argument('-f', '--filename',  dest='filename', action='store', default='default.shock', help='provide a filename, if not provided, a default is used, located in ~/.ShockStohr/default.shock')
+    parser.add_argument('-m', '--mode', dest='mode', action='store', help='Sets the mode. Acceptable modes are clip and add', required='true')
+    parser.add_argument('-u', dest='username', action='store')
+    parser.add_argument('-p', dest='password', nargs='?', action='store', default='fail')
+    parser.add_argument('-n', dest='note', nargs='?', action='store', default='fail')
+    args = parser.parse_args(sys.argv[1:])
     return args
 
 def main():
     args = handleArgs()
-    [print(args[i]) for i in args]
+    filepath = ""
+    if args.filename != "default.shock":
+        filepath = "./" + args.filename + ".shock"
+    else:
+        filepath = "~/.ShockStohr/" + args.filename
 
+    print(args)
+    if args.mode == 'add':
+        if args.note  == 'fail':
+
+            print('Please provide a note')
+            exit()
+        elif args.password == 'fail':
+            print('Please provdice a password')
+            exit()
+        else:
+            addPassword(filepath, args.username, args.password, args.note)
+            print("added the password for %s to the database" % (args.username))
+
+
+def extractPasswordList(filepath, username):
+    passwordlist = []
+    with open(filepath, r) as inputfile:
+        for line in inputfile:
+            if usename in line:
+                passwordlist.append(line)
+
+
+def addPassword(filepath, username, password, note):
+    with open(filepath, "a") as outputfile:
+        outputfile.write(username + ", " + password + ", " + note + "\n" )
 
 main()
